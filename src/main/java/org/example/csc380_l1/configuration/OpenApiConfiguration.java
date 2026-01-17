6d9603e846aed1b3e5970dfa13d5832399e458df
LissAponte <japonterivera@student.neumont.edu>
2026-01-16 20:14:23 -0700

    Imports not working in config

--- src/main/java/org/example/csc380_l1/configuration/OpenApiConfiguration.java
+++ src/main/java/org/example/csc380_l1/configuration/OpenApiConfiguration.java
@@ -1,4 +1,38 @@
 package org.example.csc380_l1.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
 public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenApiConfiguration()
+                .info(new Info()
+                        .title("Retro Video Game Exchange API")
+                        .description("REST API for managing and trading retro video games")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("API Support")
+                                .email("support@retrogameexchange.com")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Development server")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT token obtained from /login endpoint")));
+    }
 }
